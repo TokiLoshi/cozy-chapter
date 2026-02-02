3e2b2dcfa3bdde4d43a80bf5c84206e29891ea56
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2026-02-02 13:23:39 -0800

    fixing 499 error

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -4,7 +4,7 @@ import {
   generateUploadDropzone,
 } from '@uploadthing/react'
 
-import type { UploadRouter } from '../lib/server/uploadthing'
+import type { UploadRouter } from '@/lib/server/uploadthing'
 
 export const UploadButton = generateUploadButton<UploadRouter>()
 export const UploadDropzone = generateUploadDropzone<UploadRouter>()
--- src/routes/api/uploadthing.ts
+++ src/routes/api/uploadthing.ts
@@ -1,21 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
-// import { createFileRoute } from '@tanstack/react-router'
-import { createAPIFileRoute } from '@tanstack/react-start/api'
 import { createRouteHandler } from 'uploadthing/server'
-import { uploadRouter } from '../../lib/server/uploadthing'
+import { uploadRouter } from '@/lib/server/uploadthing'
 
-const handlers = createRouteHandler({ router: uploadRouter })
+const handler = createRouteHandler({ router: uploadRouter })
 
-export const Route = createAPIFileRoute('/api/uploadthing')({
+export const Route = createFileRoute('/api/uploadthing')({
   server: {
     handlers: {
-      GET: ({ request }) => {
-        console.log('==== uploadt thing GET request has been hit', request)
-        return handlers(request)
+      GET: async ({ request }) => {
+        const response = await handler(request)
+        return response
       },
-      POST: ({ request }) => {
-        console.log('===== upload thing POST request has been made', request)
-        return handlers(request)
+      POST: async ({ request }) => {
+        return handler(
+          new Request(request.url, {
+            method: request.method,
+            body: request.body,
+            headers: request.headers,
+            // This is a work around from Tanstack's Discord (courtesy of Andy Coupe)
+            // Omit the signal as it's getting prematurely triggered
+            // signal: request.signal,
+          }),
+        )
       },
     },
   },
